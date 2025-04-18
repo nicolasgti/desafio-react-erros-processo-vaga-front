dffd90eb6c1264f9b21eb29cbef529e6d6dba732
Nicolas Lima <nicolasgti@hotmail.com>
2025-04-18 16:19:28 -0400

    🎨 feat: improve code style and add fallbacks

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,8 +3,9 @@ import logo from "../../assets/logo.svg";
 
 export function Header() {
   return (
-    <header className={styles.header}>
-      <img src={logo} alt="todo" />
+    <header className={styles.header} role="banner"> //reforço da semantica, adiçao de um fallback e remocao de alt desnecessario
+      <img src={logo} alt="Logotipo da aplicação Todo" onError={(e) => (e.currentTarget.style.display = 'none')} />
+      <strong style={{ display: 'none' }}>Todo App</strong>
     </header>
   );
 }
