2ea55463fc515c3e753826139fcc4d578d0f7ddc
Nicolas Lima <nicolasgti@hotmail.com>
2025-04-18 16:23:17 -0400

    🐛 fix: fixed element Id

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
 import "./global.css";
+//<React.StrictMode> para ajudar a identificar problemas potenciais no código, como práticas obsoletas ou erros silenciosos.
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <App />
+ReactDOM.createRoot(document.getElementById("main") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
